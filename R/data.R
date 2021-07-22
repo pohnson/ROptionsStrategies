99efad6fa902bfded67be660e61b88438265150b
Pohnson <pohnson@outlook.com>
2021-07-22 11:13:36 -0700

    Commit for SP500 Data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' 504 SP500 stocks
+#'
+#' A dataset containing ticker, company name, and industry of 504 stocks in SP500
+#'
+#' @format A data frame with 504 rows and 3 variables:
+#' \describe{
+#'   \item{Ticker}{Ticker}
+#'   \item{Company}{Company Name}
+#'   \item{Industry}{Industry}
+#'   \item{Updated_Date}{Date}
+#' }
+#' @source \url{https://www.liberatedstocktrader.com/sp-500-companies-list-alphabetically-sorted/}
+'sp500'
