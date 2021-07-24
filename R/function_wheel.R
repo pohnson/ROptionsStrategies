e432f9e0d1d7791b0113fd3ec82f361507ae6192
Pohnson <pohnson@outlook.com>
2021-07-24 09:07:02 -0700

    Added 1 check and function's core.

--- R/function_wheel.R
+++ R/function_wheel.R
@@ -1,10 +1,20 @@
 #' @export
 #' @importFrom magrittr %>%
+#' @import httr
 
 wheel <- function(
-  ticker = NA,
-  expiration_date = NA,
-  td_key = NA
+  ticker = c('AAPL','GOOG'),
+  expiration_date = Sys.Date(),
+  consumer_key = NA
 ){
 
+  #Check if ticker input is in a correct format
+  if(class(ticker) != 'character'){
+    print('x')
+    stop('The input is not in a correct format. Please input either in vector or data frame with $ specifying a ticker column.')
+  }
+
+  #Check if date is in a correct format
+
+  print('y')
 }
