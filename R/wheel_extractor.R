0063a2aa42986a28f86ece8c6f3dba72ebd76493
Pohnson <pohnson@outlook.com>
2021-07-24 16:20:31 -0700

    Change name and add checking consumer key and date

--- R/wheel_extractor.R
+++ R/wheel_extractor.R
@@ -2,20 +2,27 @@
 #' @importFrom magrittr %>%
 #' @import httr
 
-wheel_selector <- function(
+wheel_extractor <- function(
   ticker = c('AAPL','GOOG'),
   expiration_date = Sys.Date(),
-  consumer_key = NA
+  consumer_key = NULL
 ){
 
   #Check if ticker input is in a correct format
   if(class(ticker) != 'character'){
-    print('x')
     stop('The input is not in a correct format. Please input either in vector or data frame with $ specifying a ticker column.')
   } # End of ticker check if
 
   #Check if date is in a correct format
+  if(class(try(as.Date(expiration_date))) == 'try-error'){
+    stop('Incorrect input expiration date format. Please input expiration date in YYYY-MM-DD format.')
+  }
 
+  #Check if consumer key is blank
+  if(is.na(consumer_key) == TRUE || is.null(consumer_key) == TRUE){
+    stop('Please input consumer key from TDAmertirade Developer.')
+  }
 
+  print(expiration_date)
   print('The code is run until the end.')
 } # End of function
