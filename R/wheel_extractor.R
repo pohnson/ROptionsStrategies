d29a085ccdaf2ec40e93b77cd64fd37a70bfd3c6
Pohnson <pohnson@outlook.com>
2021-07-24 20:14:12 -0700

    Added a lot more code and it's pretty much done

--- R/wheel_extractor.R
+++ R/wheel_extractor.R
@@ -1,11 +1,25 @@
+#' Bind two factors
+#'
+#' Create a new factor from two existing factors, where the new factor's levels
+#' are the union of the levels of the input factors.
+#'
+#' @param ticker character
+#' @param expiration_date date
+#' @param consumer_key character
+#' @param sleep_second integer
+#'
+#' @return factor
 #' @export
 #' @importFrom magrittr %>%
 #' @import httr
+#' @importFrom rlang .data
+
 
 wheel_extractor <- function(
   ticker = c('aapl','goog'),
   expiration_date = Sys.Date(),
-  consumer_key = NULL
+  consumer_key = NULL,
+  sleep_second = 1
 ){
 
   #Check if ticker input is in a correct format
@@ -39,7 +53,7 @@ wheel_extractor <- function(
     url_api_key_1.1 <- consumer_key
     url_1.2 <- '&symbol='
     url_stock_2 <- ticker[loop_a]
-    url_3 <- '&contractType=PUT&includeQuotes=TRUE&strategy=ANALYTICAL&range=OTM&fromDate='
+    url_3 <- '&contractType=PUT&includeQuotes=TRUE&strategy=ANALYTICAL&range=ALL&fromDate='
     url_from_4 <- expiration_date
     url_5 <- '&toDate='
     url_to_6 <- expiration_date
@@ -67,11 +81,66 @@ wheel_extractor <- function(
     } #End Check for Status Code
 
     ##### Extract Content #####
-    temp_data_2 <<- httr::content(temp_data)
+    temp_data_2 <- httr::content(temp_data)
+
+    ##### Extracting Data from List #####
+    temp_container_2 <- data.frame(NULL)
+    if(temp_data_2[[2]] == 'SUCCESS'){
+
+      temp_data_3 <- temp_data_2[c('putExpDateMap')]
+
+      for(loop_b in 1:length(temp_data_3)){
+        y <- list()
+
+        for(loop_c in 1:length(temp_data_3[[loop_b]])){ #at date time level
+
+          for(loop_d in 1:length(temp_data_3[[loop_b]][[loop_c]])){ #at strike price level
+            y <- append(
+              y,
+              temp_data_3[[loop_b]][[loop_c]][[loop_d]] #data at strike level
+            )
+
+          } #End of loop_d
+
+          ##### Get Only Desired Columns #####
+          y_2 <- lapply(y, helper_list_to_select)
+
+          y_3 <- data.table::rbindlist(y_2, fill = T) %>%
+            dplyr::mutate(ticker = temp_data_2$symbol)
+
+        } # End of loop_c
+
+        #Combine Data from Each Expiration of a Given Stock
+        temp_container_2 <- rbind(temp_container_2, y_3)
+
+      }# End of loop_b
+
+    }# End If Success
+
+    #Combine Data from Different Stocks
+    temp_container <- rbind(temp_container, temp_container_2)
+
+    #Sleep to make sure that TDAmeritrade will not disconnect due to too many requests
+    Sys.sleep(sleep_second)
 
   } #End of loop_a
 
-  print('The code is run until the end.')
-  return(temp_data_2)
+  temp_container_3 <- temp_container %>%
+    dplyr::mutate(
+      prob_otm = 1 + .data$delta,
+      cash_inflow = .data$bid * 100,
+      invested_capital = .data$strikePrice*100,
+      return_on_risk = (.data$bid*100)/.data$invested_capital,
+      expiration_date = expiration_date
+    )
+
+  return(data.frame(temp_container_3))
 
 } # End of function
+
+helper_list_to_select <- function(x){
+  return(
+    x[c('bid', 'delta', 'strikePrice', 'inTheMoney')]
+  )
+}
+
