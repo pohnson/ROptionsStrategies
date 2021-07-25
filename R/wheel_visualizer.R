4061fb3d6fbbb36ff73c32e474699af7422394c3
Pohnson <pohnson@outlook.com>
2021-07-25 16:03:45 -0700

    Added first check

--- R/wheel_visualizer.R
+++ R/wheel_visualizer.R
@@ -4,7 +4,7 @@
 #' what options is the best for short put also known as "The Wheel" strategy
 #'
 #' @param data xx
-#' @param visualization yy
+#' @param visualization_type yy
 #'
 #' @return
 #' A data frame object with 10 columns: bid
@@ -16,7 +16,19 @@
 
 wheel_visualizer <- function(
 data = NULL,
-visualization = c('ggplot_histogram', 'plotly_histogram')
+visualization_type = c('ggplot_histogram', 'plotly_histogram')
 ){
-  print('x')
-}
+  ##### Check if input data frame has necessary columns #####
+  if(
+    sum(
+      c('bid','delta','xxx') %in% names(data)
+    ) != 3
+  ){
+    stop('The input data frame does not have required columns: strikePrice, inTheMoney, ticker, prob_otm, cash_inflow, invested_capital, and return_on_risk')
+  }
+  print('y')
+
+
+} #End wheel_visualizer function
+
+
