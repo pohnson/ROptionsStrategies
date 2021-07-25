5317e09ae3574a183c1bd04e7e045ea4a6070e4b
Pohnson <pohnson@outlook.com>
2021-07-25 15:38:49 -0700

    First commit

---
+++ R/wheel_visualizer.R
@@ -0,0 +1,22 @@
+#' Find the Best Options for The Wheel Strategy
+#'
+#' Create a data frame with options information to determine
+#' what options is the best for short put also known as "The Wheel" strategy
+#'
+#' @param data xx
+#' @param visualization yy
+#'
+#' @return
+#' A data frame object with 10 columns: bid
+#' @export
+#' @importFrom magrittr %>%
+#' @importFrom ggplot2 ggplot geom_point
+#'
+#'
+
+wheel_visualizer <- function(
+data = NULL,
+visualization = c('ggplot_histogram', 'plotly_histogram')
+){
+  print('x')
+}
