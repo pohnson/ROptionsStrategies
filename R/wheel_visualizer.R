30d1b897a61f5743709b5ace39c8b6759135125e
Pohnson <pohnson@outlook.com>
2021-07-25 18:47:52 -0700

    Completed manual

--- R/wheel_visualizer.R
+++ R/wheel_visualizer.R
@@ -1,14 +1,30 @@
-#' Find the Best Options for The Wheel Strategy
+#' Visualize Options with the Best Return on Risk
 #'
-#' Create a data frame with options information to determine
-#' what options is the best for short put also known as "The Wheel" strategy
+#' This function will create a histogram plot for the resulting data frame from
+#' the wheel_extractor() function.
 #'
-#' @param data xx
-#' @param visualization_type yy
-#' @param include_in_the_money zz
+#' @param data A resulting data frame object from wheel_extractor() function.
+#' @param visualization_type Currently only support ggplot2's ggplot_histogram.
+#' @param include_in_the_money The Wheel generally employs on OTM options. But the data frame also includes ITM.
 #'
 #' @return
-#' A data frame object with 10 columns: bid
+#' Ggplot2 histogram chart.
+#'
+#' @examples
+#' #First let's create sample data frame
+#' sample <- data.frame(
+#'      strikePrice = c(50, 60, 70, 80),
+#'      inTheMoney = c(FALSE, FALSE, FALSE, TRUE),
+#'      ticker = c('AAPL', 'AAPL', 'TSLA', 'TSLA'),
+#'      prob_otm = c(0.3, 0.4, 0.3, 0.4),
+#'      cash_inflow = c(250, 400, 115, 220),
+#'      invested_capital = c(5000, 6000, 7000, 8000),
+#'      return_on_risk = c(0.05, 0.07, 0.02, 0.03)
+#' )
+#'
+#' #Then use the function
+#' wheel_visualizer(data = sample)
+#'
 #' @export
 #' @import ggplot2
 #' @importFrom magrittr %>%
